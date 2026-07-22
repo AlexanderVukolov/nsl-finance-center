e79c9f7c5dac8b4a26e9e72cd023c6b719a5a0ce
AlexanderVukolov <vukolov5566@gmail.com>
2026-07-23 00:23:56 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-finance-mobile-v8";
+const CACHE_NAME = "nsl-finance-mobile-v9";
 const APP_ROOT = "/nsl-finance-center/";
 const APP_SHELL = [APP_ROOT, `${APP_ROOT}index.html`];
 
@@ -59,3 +59,4 @@ self.addEventListener("fetch", event => {
     }),
   );
 });
+
