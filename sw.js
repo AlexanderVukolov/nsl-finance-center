fe89f1de94f813d98bbbf637e56eb2b9fd953d86
AlexanderVukolov <vukolov5566@gmail.com>
2026-08-13 20:55:06 +1000

    Обновить кэш P&L после добавления статуса выплат

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-finance-mobile-v12";
+const CACHE_NAME = "nsl-finance-mobile-v13";
 const APP_ROOT = "/nsl-finance-center/";
 const APP_SHELL = [APP_ROOT, `${APP_ROOT}index.html`];
 
