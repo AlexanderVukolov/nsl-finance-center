64a92b7e20317902e07c95341cbf6f8239c4c671
AlexanderVukolov <vukolov5566@gmail.com>
2026-09-02 22:59:24 +1000

    Refresh finance app cache for manager refunds

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-finance-mobile-v28";
+const CACHE_NAME = "nsl-finance-mobile-v29";
 const APP_ROOT = "/nsl-finance-center/";
 const APP_SHELL = [APP_ROOT, `${APP_ROOT}index.html`];
 
