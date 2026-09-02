d79e1efbe19fd73f0bcf5c1430c341765f541091
AlexanderVukolov <vukolov5566@gmail.com>
2026-09-03 00:10:21 +1000

    Refresh cache for P&L tax display

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-finance-mobile-v31";
+const CACHE_NAME = "nsl-finance-mobile-v32";
 const APP_ROOT = "/nsl-finance-center/";
 const APP_SHELL = [APP_ROOT, `${APP_ROOT}index.html`];
 
