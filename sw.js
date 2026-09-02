41410d977600c1402415e40354c607ca9a35905a
AlexanderVukolov <vukolov5566@gmail.com>
2026-09-03 00:53:47 +1000

    Refresh cache for P&L deduction display

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-finance-mobile-v35";
+const CACHE_NAME = "nsl-finance-mobile-v36";
 const APP_ROOT = "/nsl-finance-center/";
 const APP_SHELL = [APP_ROOT, `${APP_ROOT}index.html`];
 
