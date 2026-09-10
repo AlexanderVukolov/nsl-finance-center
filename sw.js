c31f3604cf3f1b24d6302ef75c8504d656498e6c
AlexanderVukolov <vukolov5566@gmail.com>
2026-09-10 18:43:44 +1000

    Refresh cache for faster desktop loading

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-finance-mobile-v37";
+const CACHE_NAME = "nsl-finance-mobile-v38";
 const APP_ROOT = "/nsl-finance-center/";
 const APP_SHELL = [APP_ROOT, `${APP_ROOT}index.html`];
 
