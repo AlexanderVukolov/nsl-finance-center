e31660da62ed09845637fc7b62c3374f26210ae3
AlexanderVukolov <vukolov5566@gmail.com>
2026-09-15 22:42:06 +1000

    Refresh cache after analytics number fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-finance-mobile-v45";
+const CACHE_NAME = "nsl-finance-mobile-v46";
 const APP_ROOT = "/nsl-finance-center/";
 const APP_SHELL = [APP_ROOT, `${APP_ROOT}index.html`];
 
