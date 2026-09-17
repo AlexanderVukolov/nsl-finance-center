015a1f21360f85f1261327c87181ed0ec9f76a41
AlexanderVukolov <vukolov5566@gmail.com>
2026-09-17 19:37:27 +1000

    Refresh offline app cache after sales analytics fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-finance-mobile-v48";
+const CACHE_NAME = "nsl-finance-mobile-v49";
 const APP_ROOT = "/nsl-finance-center/";
 const APP_SHELL = [APP_ROOT, `${APP_ROOT}index.html`];
 
