12f9b9c4162526aa07e1a423bf2961788777d4ad
AlexanderVukolov <vukolov5566@gmail.com>
2026-09-18 19:00:39 +1000

    Refresh August P&L archive cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-finance-mobile-v60";
+const CACHE_NAME = "nsl-finance-mobile-v61";
 const APP_ROOT = "/nsl-finance-center/";
 const APP_SHELL = [APP_ROOT, `${APP_ROOT}index.html`];
 
