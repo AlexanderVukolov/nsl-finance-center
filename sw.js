a4b62d2ea45af90d0d6ca6b7fcad7a3c8397d6db
AlexanderVukolov <vukolov5566@gmail.com>
2026-09-18 18:47:48 +1000

    Refresh cache after GetCourse panel height fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-finance-mobile-v59";
+const CACHE_NAME = "nsl-finance-mobile-v60";
 const APP_ROOT = "/nsl-finance-center/";
 const APP_SHELL = [APP_ROOT, `${APP_ROOT}index.html`];
 
