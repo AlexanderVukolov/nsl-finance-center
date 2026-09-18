3fd6bd992adddd11d0522e59cc114479d9c4f526
AlexanderVukolov <vukolov5566@gmail.com>
2026-09-18 17:08:02 +1000

    Refresh compact payroll adjustments layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "nsl-finance-mobile-v50";
+const CACHE_NAME = "nsl-finance-mobile-v51";
 const APP_ROOT = "/nsl-finance-center/";
 const APP_SHELL = [APP_ROOT, `${APP_ROOT}index.html`];
 
